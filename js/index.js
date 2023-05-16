a0ad2f1fb53dc41a837bfd9a72e162db3387919e
IvanGM <ivangm_01@hotmail.com>
2023-05-15 19:39:15 -0600

    feat: Función depósitos añadida

--- js/index.js
+++ js/index.js
@@ -3,3 +3,38 @@ saldo = parseFloat(localStorage.getItem("saldo"));
 
 document.getElementById("bienvenida").innerHTML = "Bienvenido/a " + user;
 document.getElementById("tuSaldo").innerHTML = "Tu saldo es " + saldo;
+
+const maxNumDepositos = 4;
+const maxNumRetiros = 5;
+const maxValorDepositos = 50000;
+const maxValorRetiros = 30000;
+
+let numDepositos = 0;
+let numRetiros = 0;
+let valorDepositosAcumulado = 0;
+let valorRetirosAcumulado = 0;
+
+
+function actualizarDatos(){
+    document.getElementById("tuSaldo").innerHTML = "Tu saldo es " + saldo;
+}
+
+function hacerDeposito(){
+    if(numDepositos>=maxNumDepositos){
+        alert("Has excedido los " + maxNumDepositos + " depósitos diarios permitidos")
+    }else{
+        let valorADepositar = parseFloat(document.getElementById("deposito").value);
+        if(valorDepositosAcumulado+valorADepositar>maxValorDepositos){
+            alert("No puedes depositar más de " + maxValorDepositos + " al día")
+        }else{
+            valorDepositosAcumulado += valorADepositar;
+            saldo +=valorADepositar;
+            numDepositos++;
+            actualizarDatos();
+            alert("Depósito realizado exitosamente");
+        }
+    }
+}
+
+document.getElementById("depositar").addEventListener("click", hacerDeposito);
+document.getElementById("retirar").addEventListener("click", hacerRetiro);
